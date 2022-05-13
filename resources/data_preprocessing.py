8f4ba700c82c0f6e4edaa68ec365754ce1c7e2cf
Núria Adell Raventós <nuriaadellraventos@Nurias-MacBook-Air-2.local>
2022-05-13 18:02:04 -0500

    data preprocessing file

--- resources/data_preprocessing.py
+++ resources/data_preprocessing.py
@@ -1,3 +1,7 @@
+'''
+Data preprocessing functions
+'''
+
 import pandas as pd
 import numpy as np
 
@@ -36,3 +40,21 @@ def load_referendum(path):
             data[col] = data[col].astype(float)
     
     return data
+
+def merge_dem_face_coverings(demographics, face_covering):
+    '''
+    Merge datasets of demographics and face covering. 
+    Inputs: 
+        - demographics (DF)
+        - face_covering (DF)
+    Output: DF 
+    '''
+
+    # With the inner join we lose the votes from expats (7 rows in the face_covering file)
+    merged_data = demographics.merge(
+        face_covering,
+        left_on='id', right_on='municipality_id',
+        suffixes=('_dem', '_facecov'))
+    
+    return merged_data
+
