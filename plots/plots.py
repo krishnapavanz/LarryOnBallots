6704b5af3684c300474923c8d352defbca16fd1a
Núria Adell Raventós <nuriaadellraventos@Nurias-MacBook-Air-2.local>
2022-05-26 22:51:33 -0500

    plots

---
+++ plots/plots.py
@@ -0,0 +1,25 @@
+'''
+Plots
+'''
+
+import pandas as pd
+import plotly.express as px
+
+def plot_yes_perc_error(X, y, pred):
+    '''
+    Create a scatterplot of the % yes and the error.
+    '''
+
+    ys = pd.DataFrame([X["municipality_dem"], X["yes_perc"], y, pred]).T
+    ys.columns = ['municipality', 'yes_perc', 'y_test', 'y_pred']
+    ys["error"] = ys["y_test"] - ys["y_pred"]
+    
+    fig = px.scatter(ys, x="yes_perc", y="error", color="y_test", 
+                    hover_data=["municipality"], title="% Yes & Error Rate",
+                    labels={
+                        "yes_perc": "% Yes",
+                        "error": "Error",
+                        "y_test": "True Y",
+                        "municipality": "Municipality"
+                    })
+    return fig
