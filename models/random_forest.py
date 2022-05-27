e6112c3f04cc3ec234fb037460513dbee0a21ede
Núria Adell Raventós <nuriaadellraventos@Nurias-MacBook-Air-2.local>
2022-05-27 14:43:30 -0500

    test

--- models/random_forest.py
+++ models/random_forest.py
@@ -138,5 +138,6 @@ best_parameters, best_accuracy, accuracies = random_forest(X_train, X_dev, y_tra
 
 plot_random_forest(accuracies)
 print(predict_random_forest(best_parameters, X_train, y_train, np.array(X[:2, :]), random_state = True))
-'''
+
 print(predict_random_forest(best_parameters, X_train, y_train, np.array([X[0, :]]), random_state = True))
+'''
